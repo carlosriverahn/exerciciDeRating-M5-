2887bc170dc8ef4d4738cf3551d2f28c7af31e3b
carlosriverahn <carlosriverahn@gmail.com>
2020-11-22 23:07:09 +0100

    Codigo JS y id en etiqueta Div principal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 'use strict'
-let inputs = document.querySelectorAll('.input');
+let inputs = document.querySelectorAll('input[type="radio"]');
+
+// Opcion1
 window.addEventListener("click", (event)=>{ 
-    console.log(event.target.value);
-    if(event.target.value ==  undefined){
+    console.log(event.target);
+    if(!document.getElementById("rate1").contains(event.target)){
         inputs[0].checked = true
     }
 });
+
+// Opcion2
+// window.addEventListener("click", (event)=>{ 
+//     console.log(event.target.value);
+//     if(event.target.value ==  undefined){
+//         inputs[0].checked = true
+//     }
+// });
